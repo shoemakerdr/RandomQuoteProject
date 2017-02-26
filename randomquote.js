676b5d210f5737ffa61462ae3d5c07c02855d09f
Derek Shoemaker <shoemakerdr@gmail.com>
2017-02-25 22:50:01 -0600

    wrote list of requirements for this web app in .js file and added new quotes json file

---
+++ randomquote.js
@@ -0,0 +1,16 @@
+/*
+What we need for this app:
+
+A JSON file/API with lots of quotes
+
+A QuoteDisplay object that has properties of "quote" and "author" and get/set
+methods for these properties.
+
+Manipulate the following DOM elements:
+- quote text
+- author text
+- tweet button
+- new quote button
+
+
+*/
